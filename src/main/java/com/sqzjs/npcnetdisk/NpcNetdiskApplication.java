78b1d1fb0247bc848bd420624db6b6380fad50aa
sqzjs <1150215227@qq.com>
2023-05-03 19:58:16 +0800

    init program

---
+++ src/main/java/com/sqzjs/npcnetdisk/NpcNetdiskApplication.java
@@ -0,0 +1,13 @@
+package com.sqzjs.npcnetdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcNetdiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpcNetdiskApplication.class, args);
+    }
+
+}
