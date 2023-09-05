3c80a79bc3a419eca41e77788f73c8c1f63d9101
sqzjs <1150215227@qq.com>
2023-09-05 14:50:37 +0800

    function 1.0.1 修改了错别字

--- src/main/java/com/sqzjs/npcnetdisk/NpcNetdiskApplication.java
+++ src/main/java/com/sqzjs/npcnetdisk/NpcNetdiskApplication.java
@@ -1,11 +1,12 @@
 package com.sqzjs.npcnetdisk;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sqzjs.npcnetdisk.Dao")
 public class NpcNetdiskApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NpcNetdiskApplication.class, args);
     }
